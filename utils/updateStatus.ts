1ebe68d1fdad437e1225b47ed8f6ce549db92c81
alessandro <alessandrofan94@gmail.com>
2023-08-21 22:01:49 +0200

    fix: update status function

--- utils/updateStatus.ts
+++ utils/updateStatus.ts
@@ -4,15 +4,33 @@ export async function updateOrder(updatedData: {
   channelOrderId: number;
   status: number;
 }) {
-  const { data, error } = await supabase.functions.invoke(
-    "deliverect-order-status-update",
-    {
-      body: { ...updatedData },
-    }
-  );
-
-  if (error) {
-    throw new Error("Network response was not ok");
+  try {
+    await fetch(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/deliverect-order-status-update`,
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+        },
+        body: JSON.stringify({ ...updatedData }),
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
-  return data;
+  //   const { data, error } = await supabase.functions.invoke(
+  //     "deliverect-order-status-update",
+  //     {
+  //       body: { ...updatedData },
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "no-cors": "true",
+  //       },
+  //     }
+  //   );
+
+  //   if (error) {
+  //     throw new Error("Network response was not ok");
+  //   }
+  //   return data;
 }
