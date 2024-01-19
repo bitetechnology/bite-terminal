476f63e1575a8fbe0fd19f1c97808e36b0617f1f
Alessandro Fan <alessandrofan94@gmail.com>
2024-01-19 18:34:02 +0900

    refactor: remove redifine the items in deliverect order

--- components/OrderDetail/index.tsx
+++ components/OrderDetail/index.tsx
@@ -1,5 +1,3 @@
-import { NewspaperIcon } from "@heroicons/react/24/outline";
-
 export default function OrderDetail({
   deliverect_order,
   orderId,
@@ -19,34 +17,40 @@ export default function OrderDetail({
       <h2 className="text-sm font-bold leading-7 text-gray-900 sm:truncate ">
         {`Items`}
       </h2>
-      {deliverect_order.items.map((item) => {
-        return (
-          <>
-            <div className="flex gap-x-6">
-              <div className="flex-auto">
-                <div className="flex items-start gap-x-3">
-                  <div className="text-sm font-medium leading-6 text-gray-900">
-                    {`${item.quantity} ${item.name}`}
+      {deliverect_order.items.map(
+        (item: {
+          quantity: number;
+          name: string;
+          subItems: { id: string; quantity: number; name: string }[];
+        }) => {
+          return (
+            <>
+              <div className="flex gap-x-6">
+                <div className="flex-auto">
+                  <div className="flex items-start gap-x-3">
+                    <div className="text-sm font-medium leading-6 text-gray-900">
+                      {`${item.quantity} ${item.name}`}
+                    </div>
                   </div>
+                  {item.subItems.length > 0 &&
+                    item.subItems.map((subItem) => {
+                      return (
+                        <div
+                          key={subItem.id}
+                          className="pl-4 text-xs font-medium leading-6 text-gray-900"
+                        >
+                          {`${subItem.quantity} ${subItem.name}`}
+                        </div>
+                      );
+                    })}
                 </div>
-                {item.subItems.length > 0 &&
-                  item.subItems.map((subItem) => {
-                    return (
-                      <div
-                        key={subItem.id}
-                        className="pl-4 text-xs font-medium leading-6 text-gray-900"
-                      >
-                        {`${subItem.quantity} ${subItem.name}`}
-                      </div>
-                    );
-                  })}
               </div>
-            </div>
-            <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" />
-            <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
-          </>
-        );
-      })}
+              <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" />
+              <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
+            </>
+          );
+        }
+      )}
     </>
   );
 }
